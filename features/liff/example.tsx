772a231a95f05de2648a9af1bcbbb2a367e8e66a
Jake <tjd1emd@naver.com>
2024-09-27 16:00:46 +0900

    Initial commit

---
+++ features/liff/example.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Liff } from "@line/liff";
+import { useEffect, useState } from "react";
+
+export default function LiffExample() {
+  const [liffObject, setLiffObject] = useState<Liff | null>(null);
+  const [liffError, setLiffError] = useState<string | null>(null);
+
+  // Execute liff.init() when the app is initialized
+  useEffect(() => {
+    // to avoid `window is not defined` error
+    import("@line/liff")
+      .then((liff) => liff.default)
+      .then((liff) => {
+        console.log("LIFF init...");
+        liff
+          .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID! })
+          .then(() => {
+            console.log("LIFF init succeeded.");
+            setLiffObject(liff);
+          })
+          .catch((error: Error) => {
+            console.log("LIFF init failed.");
+            setLiffError(error.toString());
+          });
+      });
+  }, []);
+
+  return (
+    <div>
+      {liffObject && (
+        <div>
+          liff created: <code>{JSON.stringify(liffObject, null, 2)}</code>
+        </div>
+      )}
+      {liffError && <code>{JSON.stringify(liffError, null, 2)}</code>}
+    </div>
+  );
+}
